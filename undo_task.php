928210493a79200e36fd7cead35a084c0b40619a
PanzerWeb <selwynvillar@gmail.com>
2024-08-30 14:25:44 +0800

    Added Undo Button
    Created an anchor HTML element for the action of reverting, and undoing a task that is set to be 'Done'
    
    Created a file undo_task.php to handle the revert function of a task.

---
+++ undo_task.php
@@ -0,0 +1,11 @@
+<?php
+    require_once "./config/database.php";
+
+    if(isset($_GET['task_id'])){
+        $task_id = $_GET["task_id"];
+
+        $revert_task = mysqli_query($connection, "UPDATE `task` SET `status` = 'Pending' WHERE `task_id` = $task_id") or die(mysqli_error($connection));
+
+        header("location: dashboard.php");
+    }
+?>
